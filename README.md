7e9f3bdc79f97bb2d0f3a0790c3849d6e3a56226
Fionzera <fionzera@gmail.com>
2022-04-30 11:01:11 -0300

    qualquer coisa

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # exercicio-curso
 Exercício proposto no curso do integrado
+
+# Heading 1
+
+qualquer coisa
+
+## Heading 2
+
+Qualquer coisa denovo
+
+- item 1
+- item 2
+- item 3
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    for(int loop=10; loop <=0; loop--){
+        cout << loop << "   ";
+    }
+
+    return 0;
+}
